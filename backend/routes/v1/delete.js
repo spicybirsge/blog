68f5a8693697c8bdf451b6078167c2e1cc76a547
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2024-06-03 10:54:43 +0530

    blog backend completed

--- backend/routes/v1/delete.js
+++ backend/routes/v1/delete.js
@@ -1,4 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const blogPosts = require("../../database/schemas/blogPosts");
+const authorize = require("../../middleware/authorize")
+
+router.delete('/blog', authorize, async(req, res) => {
+try {
+    const {id} = req.body;
+    if(!id) {
+        return res.status(400).json({success:false, message:"blog id is required", code:400})
+    }
+    const blogExist = await blogPosts.findOne({_id :id})
+    if(!blogExist) {
+        return res.status(404).json({success:false, message:"Blog post not found", code:404})
+    }
+    await blogPosts.deleteOne({_id: id});
+    return res.status(200).json({success:true, message:"Blog post deleted", code: 200})
+ } catch(e) {
+    console.error(e)
+    return res.status(500).json({success: false, message: "Internal server error", code: 500})
+}
+
+})
 
 module.exports = router;
--- backend/routes/v1/update.js
+++ backend/routes/v1/update.js
@@ -1,4 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const blogPosts = require("../../database/schemas/blogPosts");
+const authorize = require("../../middleware/authorize")
+
+
+router.put("/blog", authorize, async(req, res) => {
+try {
+    const {id, title,description, content} = req.body;
+    
+    if(!id || !title || !description || !content) {
+        return res.status(400).json({success: false, message: "id, description, content is required", code:400})
+    }
+
+    const blogPost = await blogPosts.findOne({_id: id})
+
+    if(!blogPost) {
+        return res.status(404).json({success:false, message: "Blog post not found", code:404})
+    }
+
+    blogPost.title = title;
+    blogPost.description = description;
+    blogPost.content = content;
+    blogPost.lastEdited = Date.now();
+    await blogPost.save();
+    return res.status(200).json({success: true, message: "Blog post updated", code:200})
+
+
+} catch(e) {
+    console.error(e)
+    return res.status(500).json({success: false, message: "Internal server error", code: 500})
+    
+} 
+})
+
 
 module.exports = router;
