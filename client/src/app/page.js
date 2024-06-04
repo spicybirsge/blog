e29f6f28a4a388cd1ea81753dde06a7c9cb40af4
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2024-06-04 09:51:59 +0530

    A

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -5,7 +5,7 @@ import { authenticate } from "@/functions/authenticate";
 import LoadingComponent from "@/components/LoadingComponent";
 import {Card } from 'antd';
 import Navbar from "@/components/NavBar";
-import {Typography,  Divider} from 'antd'
+import {Typography,  Divider, Spin, message} from 'antd'
 const {Title, Paragraph, Text} = Typography;
 import './global.css'
 import PostCard from "@/components/PostCard";
@@ -14,7 +14,8 @@ export default function Page() {
   const isLoaded = loaded((state) => state.loaded);
   const isLoggedIn = loggedIn((state) => state.loggedIn);
  const [posts, setPosts] = useState([])
-
+const [postFetching, setPostsFetching] = useState(true)
+const [messageApi, contextHolder] = message.useMessage();
   useEffect(() => {
    
     if (!isLoaded) {
@@ -28,8 +29,15 @@ export default function Page() {
       })
 
       const response = await request.json()
-      setPosts(response.data)
-      return;
+  
+      if(response.success) {
+        setPostsFetching(false)
+        setPosts(response.data)
+        return;
+      } else {
+      return messageApi.success("Error fetching posts")
+      }
+     
         
     }
     if(isLoaded) {
@@ -44,15 +52,16 @@ export default function Page() {
     return <LoadingComponent/>
   }
 
-  return <><Navbar loggedIn={isLoggedIn}></Navbar>
+  return <>{contextHolder}<Navbar loggedIn={isLoggedIn}></Navbar>
   <div className="container">
     <Title level={2}>Shaheer Ahamed's Blog</Title>
     <Paragraph>Hi I am Shaheer Ahamed a full-stack web and app developer. You can read my blog posts here. Want to contact me? message me through <a href="https://discord.com/users/818903544723406858" target="_blank">discord</a>.</Paragraph>
     <Divider></Divider>
     <Title level={2} style={{"textAlign": "center"}}>Posts</Title>
+    <Spin spinning={postFetching} size="large">
     {posts.map((blogpost)=> (<>
       <PostCard createdAt={blogpost.createdAt} title={blogpost.title} description={blogpost.description} loggedIn={isLoggedIn} id={blogpost._id} views={blogpost.views}></PostCard> <Divider/></>
-    ) ) }
+    ) ) }{posts.length === 0 ? <Paragraph style={{"textAlign": "center"}}>No posts yet, come back later?</Paragraph> : <></>}</Spin>
 
    </div></>;
 }
