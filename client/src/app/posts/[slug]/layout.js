4f7a689ba11997f461dd0b175e4b357e6f163aa4
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2024-06-07 20:11:21 +0530

    fixed metatags

--- client/src/app/posts/[slug]/layout.js
+++ client/src/app/posts/[slug]/layout.js
@@ -4,42 +4,60 @@ import { AntdRegistry } from '@ant-design/nextjs-registry';
 import vars from "@/variables/vars";
 const inter = Inter({ subsets: ["latin"] });
 
+export async function generateMetadata({ params, searchParams }, parent) {
+ 
+  const id = params.slug
+  const url = vars.BACKEND_URL+"/api/v1/read/blog?id="+id
+  const request = await fetch(url, {
+      method: 'GET',
+      next: {
+        revalidate: 60
+      }
+  })
 
-export const metadata = {
-    title: `Read blog`,
-    description: "Read a blog on Shaheer Ahamed's  blog page",
-    icons: {
-      icon: '/logo.png',
-      shortcut: '/logo.png',
-      apple: '/logo.png',
-      other: {
-        rel: 'apple-touch-icon-precomposed',
-        url: '/logo.png',
-      },
-    },
-    openGraph: {
-      title: 'Read blog',
-      description: "Read a blog on Shaheer Ahamed's  blog page",
+  const response = await request.json();
+ 
     
-      siteName: "Shaheer Ahamed's Blog",
-      images: [
-        {
-          url: '/logo.png',
-          width: 479,
-          height: 472 ,
-          alt: 'Logo',
+      
+      return {
+        title: response.data.title,
+        description: response.data.description,
+        icons: {
+          icon: '/logo.png',
+          shortcut: '/logo.png',
+          apple: '/logo.png',
+          other: {
+            rel: 'apple-touch-icon-precomposed',
+            url: '/logo.png',
+          },
         },
-      ],
-      locale: 'en_US',
-      type: 'website',
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title: "Shaheer Ahamed's Blog",
-      description: "Read a blog on Shaheer Ahamed's  blog page",
-      images: ['/logo.png'],
-    },
-  };
+        openGraph: {
+          title: response.data.title,
+          description: response.data.description,
+        
+          siteName: "Shaheer Ahamed's Blog",
+          images: [
+            {
+              url: '/logo.png',
+              width: 479,
+              height: 472 ,
+              alt: 'Logo',
+            },
+          ],
+          locale: 'en_US',
+          type: 'website',
+        },
+        twitter: {
+          card: 'summary_large_image',
+          title: response.data.title,
+          description: response.data.description,
+          images: ['/logo.png'],
+        },
+      };
+
+  
+
+}
 
 
 export default function PostLayout({ children }) {
